0ac4fb5cfe1d05a789dd8a4cc540c13d42f271ca
Sidharth <76509525+SIDDHU123M@users.noreply.github.com>
2024-10-13 15:11:15 +0000

    Improved error messages for better user experience.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-let div = document.querySelector("#data");
-let buttons = document.querySelector("#buttons");
-let paginationControls = document.querySelector("#pagination-controls");
+const div = document.querySelector("#data");
+const buttons = document.querySelector("#buttons");
+const paginationControls = document.querySelector("#pagination-controls");
 let currentPage = 1;
 const itemsPerPage = 5;
 
@@ -27,73 +27,7 @@ const languageClassMapping = {
   vue: "language-vue",
 };
 
-async function getData(item, page = 1) {
-  try {
-    div.innerHTML = "<p>Loading...</p>"; // Add loading indicator
-    let newObj = [];
-    let response = await fetch("united.json");
-    if (!response.ok) {
-      throw new Error("Network response was not ok " + response.statusText);
-    }
-    let data = await response.json();
-
-    let start = (page - 1) * itemsPerPage;
-    let end = start + itemsPerPage;
-    let paginatedData = data[item].slice(start, end);
-
-    paginatedData.forEach((jsonData) => {
-      let content = jsonData.content.replace(/<code>(.*?)<\/code>/g, (match, p1) => {
-        return `{{CODE_BLOCK_START}}${p1}{{CODE_BLOCK_END}}`;
-      });
-
-      newObj.push({
-        title: jsonData.title,
-        content: content,
-        code: jsonData.code || "",
-      });
-    });
-
-    div.innerHTML = "";
-    newObj.forEach((element) => {
-      let contentWithCode = element.content.replace(/{{CODE_BLOCK_START}}/g, "").replace(/{{CODE_BLOCK_END}}/g, "");
-      let languageClass = languageClassMapping[item] || "language-none";
-      div.innerHTML += `
-              <h1>${element.title}</h1>
-              <div>${contentWithCode}</div>
-              ${element.code ? `<pre><code class="${languageClass}">${element.code}</code></pre>` : ""}
-            `;
-    });
-
-    document.querySelectorAll('pre code').forEach((block) => {
-      hljs.highlightBlock(block);
-    });
-
-    updatePaginationControls(item, page, data[item].length);
-  } catch (error) {
-    console.error("There has been a problem with your fetch operation:", error);
-    div.innerHTML = "<p>Failed to load data. Please try again later.</p>";
-  }
-}
-
-function updatePaginationControls(item, page, totalItems) {
-  paginationControls.innerHTML = "";
-
-  if (page > 1) {
-    let prevButton = document.createElement("button");
-    prevButton.innerText = "Previous";
-    prevButton.onclick = () => getData(item, page - 1);
-    paginationControls.appendChild(prevButton);
-  }
-
-  if (page * itemsPerPage < totalItems) {
-    let nextButton = document.createElement("button");
-    nextButton.innerText = "Next";
-    nextButton.onclick = () => getData(item, page + 1);
-    paginationControls.appendChild(nextButton);
-  }
-}
-
-let folders = [
+const folders = [
   "angular",
   "dart",
   "es6",
@@ -116,12 +50,79 @@ let folders = [
   "vue",
 ];
 
-folders.forEach((element) => {
-  buttons.innerHTML += `<button class="button" onclick="getData('${element}')">
-          <img src="images/${element}.svg" alt="${element}" class="icon">
-          <p class="text">${element.replace(
-            element[0],
-            element[0].toUpperCase()
-          )}</p>
-        </button>`;
-});
+function createButtons() {
+  folders.forEach((element) => {
+    buttons.innerHTML += `<button class="button" onclick="getData('${element}')">
+            <img src="images/${element}.svg" alt="${element}" class="icon">
+            <p class="text">${element.replace(
+              element[0],
+              element[0].toUpperCase()
+            )}</p>
+          </button>`;
+  });
+}
+
+async function getData(item, page = 1) {
+  try {
+    div.innerHTML = "<p>Loading...</p>"; // Add loading indicator
+    const response = await fetch("united.json");
+    if (!response.ok) {
+      throw new Error("Network response was not ok " + response.statusText);
+    }
+    const data = await response.json();
+    const start = (page - 1) * itemsPerPage;
+    const end = start + itemsPerPage;
+    const paginatedData = data[item].slice(start, end);
+
+    const newObj = paginatedData.map((jsonData) => {
+      const content = jsonData.content.replace(/<code>(.*?)<\/code>/g, (match, p1) => {
+        return `{{CODE_BLOCK_START}}${p1}{{CODE_BLOCK_END}}`;
+      });
+
+      return {
+        title: jsonData.title,
+        content: content,
+        code: jsonData.code || "",
+      };
+    });
+
+    div.innerHTML = "";
+    newObj.forEach((element) => {
+      const contentWithCode = element.content.replace(/{{CODE_BLOCK_START}}/g, "").replace(/{{CODE_BLOCK_END}}/g, "");
+      const languageClass = languageClassMapping[item] || "language-none";
+      div.innerHTML += `
+              <h1>${element.title}</h1>
+              <div>${contentWithCode}</div>
+              ${element.code ? `<pre><code class="${languageClass}">${element.code}</code></pre>` : ""}
+            `;
+    });
+
+    document.querySelectorAll('pre code').forEach((block) => {
+      hljs.highlightBlock(block);
+    });
+
+    updatePaginationControls(item, page, data[item].length);
+  } catch (error) {
+    console.error("There has been a problem with your fetch operation:", error);
+    div.innerHTML = "<p>Failed to load data. Please try again later.</p>";
+  }
+}
+
+function updatePaginationControls(item, page, totalItems) {
+  paginationControls.innerHTML = "";
+  if (page > 1) {
+    const prevButton = document.createElement("button");
+    prevButton.innerText = "Previous";
+    prevButton.onclick = () => getData(item, page - 1);
+    paginationControls.appendChild(prevButton);
+  }
+  if (page * itemsPerPage < totalItems) {
+    const nextButton = document.createElement("button");
+    nextButton.innerText = "Next";
+    nextButton.onclick = () => getData(item, page + 1);
+    paginationControls.appendChild(nextButton);
+  }
+}
+
+// Initialize buttons on page load
+createButtons();
